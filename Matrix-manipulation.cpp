092ed9b69d577b3a5a60cff44d72b35329af5657
chandresh189 <48803866+chandresh189@users.noreply.github.com>
2019-10-09 12:27:18 +0530

    Update matrix-manipulator.cpp

--- Matrix-manipulation.cpp
+++ Matrix-manipulation.cpp
@@ -19,41 +19,10 @@ int  main()
 	cin>>x>>y;
 	x--;
 	y--;
-	#include<iostream>
-using namespace std;
-int  main()
-{
-	int t;
-	int x,y;
-	cin>>t;
-	while(t--)
-	{
-	int r,c,q,cnt=0;
-	cin>>r>>c>>q;
-	int a[r][c]={0};
-	int *p;
-	p=&a[0][0];
-	for(int i=0;i<c;i++)
-	a[0][i]=0;
-	while(q--)
-	{
-	cin>>x>>y;
-	x--;
-	y--;
-
-	}
-
-		for(int i=0;i<r*c;i++)
-	    {
-	    	int item;
-	    	item=*p;
-	    	if(item%2!=0)
-	    	cnt++;
-	    	p++;
-	    }
-	cout<<cnt<<endl;	
-	}
-}
+	for(int i=0;i<c;i++) 
+	a[x][i]++;
+	for(int j=0;j<r;j++)
+	a[j][y]++;
 	}
 
 		for(int i=0;i<r*c;i++)
